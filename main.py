0d8259c0584ef020708a5bb2e9e40a04bcd81d8a
ratva0717 <ratchana@student.tce.edu>
2021-10-27 14:02:43 +0530

    new change added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from scripts_driver.load_data import *
+import pickle
 
 app = Flask(__name__)
 
@@ -14,10 +15,22 @@ def descriptive():
     df = covid_data()
     return render_template('icons.html', label=label, data_1=data_1, data_2=data_2,index=index, data_3=data_3, data_5=data_5, data_4=data_4, df=df)
 
-@app.route('/about')
+@app.route('/predict')
 def aboutpage():
     return render_template('tables.html')
 
+@app.route('/result', methods=['GET','POST'])
+def result():
+    if request.method == 'POST':
+        data = request.form['time']
+        n = int(data[-1]) - 2
+        model_fit = pickle.load(open('model_h.pkl','rb'))
+        yhat = model_fit.predict(start = 0, end = 2)
+        model_fitm = pickle.load(open('model_m.pkl','rb'))
+        ypred = model_fitm.predict(start = 0, end = 2)
+        model_fitd = pickle.load(open('model_d.pkl','rb'))
+        ypre = model_fitd.predict(start = 0, end = 2)
+    return render_template('tables.html', anchor = "box-view", yhat=yhat, ypre=ypre, ypred=ypred, n = n)
 @app.route('/contact')
 def contactpage():
     return render_template('user.html')
